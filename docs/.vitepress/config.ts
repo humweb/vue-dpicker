4c946086f63eb5827d59915818df26164ba47054
Ryan Shofner <ryan@letsgolearn.com>
2025-06-26 10:05:14 -0700

    update docs configuration, remove unused files, and refine build settings
    Signed-off-by: Ryan Shofner <ryan@letsgolearn.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,30 +1,33 @@
-import { defineConfig } from 'vitepress'
-import vue from '@vitejs/plugin-vue'
+import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Vue dPicker",
-  description: "A Vue Datepicker component",
-  base: '/vue-dpicker/',
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Components', link: '/components/date-picker' },
-      { text: 'Examples', link: '/examples' }
-    ],
+    title: "Vue DPicker",
+    description: "A Vue Datepicker component",
+    base: '/vue-dpicker/',
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        nav: [
+            {text: 'Home', link: '/'},
+            {text: 'Components', link: '/components/date-picker'},
+            {text: 'Examples', link: '/examples'}
+        ],
 
-    sidebar: [
-      {
-        text: 'Components',
-        items: [
-          { text: 'DatePicker', link: '/components/date-picker' }
+        sidebar: [
+            {
+                text: 'Components',
+                items: [
+                    {text: 'DatePicker', link: '/components/date-picker'},
+                    {text: 'Examples', link: '/examples'}
+                ]
+            }
+        ],
+        footer: {
+            message: 'Released under the MIT License.',
+            copyright: `Copyright © ${new Date().getFullYear()} HumboldtWeb. All rights reserved.`
+        },
+        socialLinks: [
+            {icon: 'github', link: 'https://github.com/ryun-dev/vue-dpicker'}
         ]
-      }
-    ],
-
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/ryun-dev/vue-dpicker' }
-    ]
-  }
+    }
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import pkg from './package.json';
+import { fileURLToPath } from 'url'
+import pkg from './package.json'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
